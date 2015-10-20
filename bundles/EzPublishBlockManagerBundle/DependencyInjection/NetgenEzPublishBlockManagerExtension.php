cd20c76a7fe4aa5ec16df6a51589dc49123cb6b8
Edi Modrić <edi.modric@gmail.com>
2015-10-20 21:41:10 +0200

    Initial import

---
+++ bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Netgen\Bundle\EzPublishBlockManagerBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class NetgenEzPublishBlockManagerExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('serializers.yml');
+
+        $loader->load('view/template_providers.yml');
+    }
+}
---
+++ bundles/EzPublishBlockManagerBundle/NetgenEzPublishBlockManagerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Netgen\Bundle\EzPublishBlockManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NetgenEzPublishBlockManagerBundle extends Bundle
+{
+}
