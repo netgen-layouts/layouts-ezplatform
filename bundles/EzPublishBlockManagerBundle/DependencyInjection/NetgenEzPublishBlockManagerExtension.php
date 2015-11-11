4d1cb04d4048693b4661e48b9f64a9b16e724c47
Edi Modrić <edi.modric@gmail.com>
2015-11-11 21:19:53 +0100

    Add eZ Publish specific default pagelayout

--- bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
+++ bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
@@ -23,6 +23,7 @@ class NetgenEzPublishBlockManagerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('default_settings.yml');
         $loader->load('normalizers.yml');
 
         $loader->load('view/template_providers.yml');
