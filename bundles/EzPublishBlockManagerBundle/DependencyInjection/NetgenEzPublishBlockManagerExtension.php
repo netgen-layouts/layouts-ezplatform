9b32d01fc222279c901d3be78162a0ef837b272d
Edi Modrić <edi.modric@gmail.com>
2015-11-11 15:08:04 +0100

    Remove JMS serializer and use Symfony serializer component
    JMS serializer cannot be configured to serialize interfaces and abstract classes!?

--- bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
+++ bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
@@ -23,7 +23,7 @@ class NetgenEzPublishBlockManagerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('serializers.yml');
+        $loader->load('normalizers.yml');
 
         $loader->load('view/template_providers.yml');
     }
