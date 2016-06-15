c42c4480493826792a96bb6769b976428498ca08
Edi Modrić <edi.modric@gmail.com>
2016-06-15 23:54:47 +0200

    Handle sorting in ez query type

--- bundles/EzPublishBlockManagerBundle/Collection/QueryType/Handler/EzContentSearchHandler.php
+++ bundles/EzPublishBlockManagerBundle/Collection/QueryType/Handler/EzContentSearchHandler.php
@@ -5,6 +5,7 @@ namespace Netgen\Bundle\EzPublishBlockManagerBundle\Collection\QueryType\Handler
 use eZ\Publish\API\Repository\LocationService;
 use eZ\Publish\API\Repository\Values\Content\Location;
 use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
 use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
 use Netgen\BlockManager\Collection\QueryType\QueryTypeHandlerInterface;
 use eZ\Publish\API\Repository\ContentTypeService;
@@ -40,6 +41,34 @@ class EzContentSearchHandler implements QueryTypeHandlerInterface
      * @var \eZ\Publish\API\Repository\SearchService
      */
     protected $searchService;
+
+    /**
+     * @var array
+     */
+    protected $sortClauses = array(
+        'default' => SortClause\DatePublished::class,
+        'date_published' => SortClause\DatePublished::class,
+        'date_modified' => SortClause\DateModified::class,
+        'content_name' => SortClause\ContentName::class,
+        'location_priority' => SortClause\Location\Priority::class,
+        Location::SORT_FIELD_PATH => SortClause\Location\Path::class,
+        Location::SORT_FIELD_PUBLISHED => SortClause\DatePublished::class,
+        Location::SORT_FIELD_MODIFIED => SortClause\DateModified::class,
+        Location::SORT_FIELD_SECTION => SortClause\SectionIdentifier::class,
+        Location::SORT_FIELD_DEPTH => SortClause\Location\Depth::class,
+        Location::SORT_FIELD_PRIORITY => SortClause\Location\Priority::class,
+        Location::SORT_FIELD_NAME => SortClause\ContentName::class,
+        Location::SORT_FIELD_NODE_ID => SortClause\Location\Id::class,
+        Location::SORT_FIELD_CONTENTOBJECT_ID => SortClause\ContentId::class,
+    );
+
+    /**
+     * @var array
+     */
+    protected $sortDirections = array(
+        Location::SORT_ORDER_ASC => LocationQuery::SORT_ASC,
+        Location::SORT_ORDER_DESC => LocationQuery::SORT_DESC,
+    );
 
     /**
      * Constructor.
@@ -85,7 +114,7 @@ class EzContentSearchHandler implements QueryTypeHandlerInterface
                         'Modified' => 'date_modified',
                         'Alphabetical' => 'content_name',
                         'Priority' => 'location_priority',
-                        'Defined by parent' => 'parent_location',
+                        'Defined by parent' => 'defined_by_parent',
                     )
                 ),
                 true
@@ -269,6 +298,24 @@ class EzContentSearchHandler implements QueryTypeHandlerInterface
                 $parameters['limit'] :
                 self::DEFAULT_LIMIT;
         }
+
+        $sortType = 'default';
+        if (!empty($parameters['sort_type'])) {
+            $sortType = $parameters['sort_type'] === 'defined_by_parent' ?
+                $parentLocation->sortField :
+                $parameters['sort_type'];
+        }
+
+        $sortDirection = LocationQuery::SORT_DESC;
+        if (!empty($parameters['sort_direction'])) {
+            $sortDirection = isset($parameters['sort_type']) && $parameters['sort_type'] === 'defined_by_parent' ?
+                $this->sortDirections[$parentLocation->sortOrder] :
+                $parameters['sort_direction'];
+        }
+
+        $query->sortClauses = array(
+            new $this->sortClauses[$sortType]($sortDirection)
+        );
 
         return $query;
     }
