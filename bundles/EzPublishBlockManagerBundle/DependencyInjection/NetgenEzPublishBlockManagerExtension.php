b6efaab97e21b7ddf9bebe1fc8d7ceedfc0f6489
Edi Modrić <edi.modric@gmail.com>
2016-06-18 15:02:55 +0200

    Move ez source definition to ez bundle

--- bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
+++ bundles/EzPublishBlockManagerBundle/DependencyInjection/NetgenEzPublishBlockManagerExtension.php
@@ -61,6 +61,7 @@ class NetgenEzPublishBlockManagerExtension extends Extension implements PrependE
             'block_definitions.yml' => 'netgen_block_manager',
             'block_types.yml' => 'netgen_block_manager',
             'query_types.yml' => 'netgen_block_manager',
+            'sources.yml' => 'netgen_block_manager',
             'view/block_view.yml' => 'netgen_block_manager',
         );
 
