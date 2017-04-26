07861ae5950ba897b31527c1fad0ce2f7986b885
Edi Modrić <edi.modric@gmail.com>
2017-04-26 15:58:40 +0200

    Use @doesNotPerformAssertions annotation with empty container pass tests

--- tests/bundle/DependencyInjection/CompilerPass/DefaultAppPreviewPassTest.php
+++ tests/bundle/DependencyInjection/CompilerPass/DefaultAppPreviewPassTest.php
@@ -94,15 +94,11 @@ class DefaultAppPreviewPassTest extends AbstractCompilerPassTestCase
 
     /**
      * @covers \Netgen\Bundle\EzPublishBlockManagerBundle\DependencyInjection\CompilerPass\DefaultAppPreviewPass::process
+     * @doesNotPerformAssertions
      */
     public function testProcessWithEmptyContainer()
     {
         $this->compile();
-
-        $this->assertEmpty($this->container->getAliases());
-        // The container has at least self ("service_container") as the service
-        $this->assertCount(1, $this->container->getServiceIds());
-        $this->assertEmpty($this->container->getParameterBag()->all());
     }
 
     /**
