75e020ba2819117c3f67ca88ccb05b4d5337384a
Edi Modrić <edi.modric@gmail.com>
2017-05-04 15:24:58 +0200

    Update type hints

--- lib/ContentProvider/ContentProviderInterface.php
+++ lib/ContentProvider/ContentProviderInterface.php
@@ -7,14 +7,14 @@ interface ContentProviderInterface
     /**
      * Provides the eZ Publish content value object.
      *
-     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     * @return \eZ\Publish\API\Repository\Values\Content\Content|void
      */
     public function provideContent();
 
     /**
      * Provides the eZ Publish location value object.
      *
-     * @return \eZ\Publish\API\Repository\Values\Content\Location
+     * @return \eZ\Publish\API\Repository\Values\Content\Location|void
      */
     public function provideLocation();
 }
