c960ee092171efd3bd0a708478399f9e82c69400
Petar Španja <petar@spanja.info>
2017-05-05 16:13:47 +0200

    LAYOUTS-290: add simple hierarchy functionality

--- bundle/Security/Authorization/Voter/RepositoryAccessVoter.php
+++ bundle/Security/Authorization/Voter/RepositoryAccessVoter.php
@@ -6,6 +6,10 @@ use eZ\Publish\API\Repository\Repository;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
+/**
+ * Votes on Netgen Layout attributes (ngbm:*) by matching corresponding access
+ * rights in eZ Platform Repository.
+ */
 class RepositoryAccessVoter extends Voter
 {
     /**
@@ -26,6 +30,19 @@ class RepositoryAccessVoter extends Voter
     );
 
     /**
+     * Describes inverted attribute hierarchy.
+     *
+     * Only one level, explicit (no indirection is allowed).
+     *
+     * @var array
+     */
+    private static $attributeHierarchy = array(
+        'ngbm:editor' => array(
+            'ngbm:admin',
+        ),
+    );
+
+    /**
      * @var \eZ\Publish\API\Repository\Repository
      */
     private $repository;
@@ -43,10 +60,52 @@ class RepositoryAccessVoter extends Voter
         return is_string($attribute) && isset(self::$attributeToPolicyMap[$attribute]);
     }
 
+    public function vote(TokenInterface $token, $object, array $attributes)
+    {
+        // abstain vote by default in case none of the attributes are supported
+        $vote = self::ACCESS_ABSTAIN;
+
+        foreach ($attributes as $attribute) {
+            if (!$this->supports($attribute, $object)) {
+                continue;
+            }
+
+            $reachableAttributes = $this->getReachableAttributes($attribute);
+
+            // rely on vote resolved by parent implementation
+            $vote = parent::vote($token, $object, $reachableAttributes);
+
+            // return only if granted
+            if ($vote === self::ACCESS_GRANTED) {
+                return self::ACCESS_GRANTED;
+            }
+        }
+
+        return $vote;
+    }
+
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
         $function = self::$attributeToPolicyMap[$attribute];
 
         return $this->repository->hasAccess(self::$module, $function);
+    }
+
+    /**
+     * Return all attributes reachable by the given $attribute through hierarchy.
+     *
+     * @param string $attribute
+     *
+     * @return string[]
+     */
+    private function getReachableAttributes($attribute)
+    {
+        $attributes = [$attribute];
+
+        if (isset(self::$attributeHierarchy[$attribute])) {
+            $attributes = array_merge($attributes, self::$attributeHierarchy[$attribute]);
+        }
+
+        return $attributes;
     }
 }
