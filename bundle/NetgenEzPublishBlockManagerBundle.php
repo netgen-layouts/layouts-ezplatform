58df9d1ff7c47316e74e93faec528dd99bc827e1
Edi Modrić <edi.modric@gmail.com>
2017-05-09 16:27:11 +0200

    Add policy provider only if using eZ kernel 6

--- bundle/NetgenEzPublishBlockManagerBundle.php
+++ bundle/NetgenEzPublishBlockManagerBundle.php
@@ -27,6 +27,11 @@ class NetgenEzPublishBlockManagerBundle extends Bundle
 
         /** @var \eZ\Bundle\EzPublishCoreBundle\DependencyInjection\EzPublishCoreExtension $ezCoreExtension */
         $ezCoreExtension = $container->getExtension('ezpublish');
-        $ezCoreExtension->addPolicyProvider(new PolicyProvider());
+
+        // @deprecated Check for existence of method for compatibility with eZ Publish 5.4.x
+        // @todo Remove the check when support for 5.4.x ends
+        if (method_exists($ezCoreExtension, 'addPolicyProvider')) {
+            $ezCoreExtension->addPolicyProvider(new PolicyProvider());
+        }
     }
 }
