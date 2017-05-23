19aa78a959c0f71a7d7979610d72430133954a3d
Edi Modrić <edi.modric@gmail.com>
2017-05-23 16:51:37 +0200

    Remove $parameters argument from getDynamicParameters

--- lib/Block/BlockDefinition/Handler/ContentFieldHandler.php
+++ lib/Block/BlockDefinition/Handler/ContentFieldHandler.php
@@ -41,11 +41,10 @@ class ContentFieldHandler extends BlockDefinitionHandler
      * Returns the array of dynamic parameters provided by this block definition.
      *
      * @param \Netgen\BlockManager\API\Values\Block\Block $block
-     * @param array $parameters
      *
      * @return array
      */
-    public function getDynamicParameters(Block $block, array $parameters = array())
+    public function getDynamicParameters(Block $block)
     {
         return array(
             'content' => $this->contentProvider->provideContent(),
