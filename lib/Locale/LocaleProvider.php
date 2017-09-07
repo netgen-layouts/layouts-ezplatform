3969d29f82bcd6408e4a4f178578ad85971b3010
Edi Modrić <edi.modric@gmail.com>
2017-09-07 14:57:40 +0200

    Implement missing method in LocaleProvider

--- lib/Locale/LocaleProvider.php
+++ lib/Locale/LocaleProvider.php
@@ -2,9 +2,11 @@
 
 namespace Netgen\BlockManager\Ez\Locale;
 
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use eZ\Publish\API\Repository\LanguageService;
 use eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface;
 use Netgen\BlockManager\Locale\LocaleProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Intl\Intl;
 
 class LocaleProvider implements LocaleProviderInterface
@@ -20,6 +22,16 @@ class LocaleProvider implements LocaleProviderInterface
     protected $localeConverter;
 
     /**
+     * @var \Symfony\Component\Intl\ResourceBundle\LocaleBundleInterface
+     */
+    protected $localeBundle;
+
+    /**
+     * @var string[]
+     */
+    protected $languageCodes = array();
+
+    /**
      * Constructor.
      *
      * @param \eZ\Publish\API\Repository\LanguageService $languageService
@@ -29,6 +41,17 @@ class LocaleProvider implements LocaleProviderInterface
     {
         $this->languageService = $languageService;
         $this->localeConverter = $localeConverter;
+        $this->localeBundle = Intl::getLocaleBundle();
+    }
+
+    /**
+     * Sets the available language codes to the provider.
+     *
+     * @param string[] $languageCodes
+     */
+    public function setLanguages(array $languageCodes = null)
+    {
+        $this->languageCodes = !empty($languageCodes) ? $languageCodes : array();
     }
 
     /**
@@ -41,23 +64,56 @@ class LocaleProvider implements LocaleProviderInterface
     public function getAvailableLocales()
     {
         $availableLocales = array();
-        $localeBundle = Intl::getLocaleBundle();
-
         $languages = $this->languageService->loadLanguages();
 
         foreach ($languages as $language) {
-            if ($language->enabled) {
-                $posixLocale = $this->localeConverter->convertToPOSIX($language->languageCode);
-                $localeName = $localeBundle->getLocaleName($posixLocale);
+            if (!$language->enabled) {
+                continue;
+            }
 
-                if ($localeName !== null) {
-                    $availableLocales[$posixLocale] = $localeName;
-                }
+            $posixLocale = $this->localeConverter->convertToPOSIX($language->languageCode);
+            $localeName = $this->localeBundle->getLocaleName($posixLocale);
+
+            if ($localeName !== null) {
+                $availableLocales[$posixLocale] = $localeName;
             }
         }
 
         asort($availableLocales);
 
         return $availableLocales;
+    }
+
+    /**
+     * Returns the list of locale codes available for the provided request.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return string[]
+     */
+    public function getRequestLocales(Request $request)
+    {
+        $requestLocales = array();
+
+        foreach ($this->languageCodes as $languageCode) {
+            try {
+                $language = $this->languageService->loadLanguage($languageCode);
+            } catch (NotFoundException $e) {
+                continue;
+            }
+
+            if (!$language->enabled) {
+                continue;
+            }
+
+            $posixLocale = $this->localeConverter->convertToPOSIX($language->languageCode);
+            $localeName = $this->localeBundle->getLocaleName($posixLocale);
+
+            if ($localeName !== null) {
+                $requestLocales[] = $posixLocale;
+            }
+        }
+
+        return $requestLocales;
     }
 }
--- tests/lib/Locale/LocaleProviderTest.php
+++ tests/lib/Locale/LocaleProviderTest.php
@@ -4,9 +4,11 @@ namespace Netgen\BlockManager\Ez\Tests\Locale;
 
 use eZ\Publish\API\Repository\LanguageService;
 use eZ\Publish\API\Repository\Values\Content\Language;
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
 use eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface;
 use Netgen\BlockManager\Ez\Locale\LocaleProvider;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 
 class LocaleProviderTest extends TestCase
 {
@@ -72,5 +74,70 @@ class LocaleProviderTest extends TestCase
 
         $this->assertEquals(array('hr', 'en'), array_keys($availableLocales));
         $this->assertEquals(array('Croatian', 'English'), array_values($availableLocales));
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\Ez\Locale\LocaleProvider::setLanguages
+     * @covers \Netgen\BlockManager\Ez\Locale\LocaleProvider::getRequestLocales
+     */
+    public function testGetRequestLocales()
+    {
+        $this->localeProvider->setLanguages(array('eng-GB', 'ger-DE', 'unknown', 'cro-HR'));
+
+        $this->languageServiceMock
+            ->expects($this->at(0))
+            ->method('loadLanguage')
+            ->with($this->equalTo('eng-GB'))
+            ->will(
+                $this->returnValue(
+                    new Language(array('languageCode' => 'eng-GB', 'enabled' => true))
+                )
+            );
+
+        $this->languageServiceMock
+            ->expects($this->at(1))
+            ->method('loadLanguage')
+            ->with($this->equalTo('ger-DE'))
+            ->will(
+                $this->returnValue(
+                    new Language(array('languageCode' => 'ger-DE', 'enabled' => false))
+                )
+            );
+
+        $this->languageServiceMock
+            ->expects($this->at(2))
+            ->method('loadLanguage')
+            ->with($this->equalTo('unknown'))
+            ->will(
+                $this->throwException(
+                    new NotFoundException('language', 'unknown')
+                )
+            );
+
+        $this->languageServiceMock
+            ->expects($this->at(3))
+            ->method('loadLanguage')
+            ->with($this->equalTo('cro-HR'))
+            ->will(
+                $this->returnValue(
+                    new Language(array('languageCode' => 'cro-HR', 'enabled' => true))
+                )
+            );
+
+        $this->localeConverterMock
+            ->expects($this->at(0))
+            ->method('convertToPOSIX')
+            ->with($this->equalTo('eng-GB'))
+            ->will($this->returnValue('en'));
+
+        $this->localeConverterMock
+            ->expects($this->at(1))
+            ->method('convertToPOSIX')
+            ->with($this->equalTo('cro-HR'))
+            ->will($this->returnValue('hr'));
+
+        $requestLocales = $this->localeProvider->getRequestLocales(Request::create(''));
+
+        $this->assertEquals(array('en', 'hr'), $requestLocales);
     }
 }
