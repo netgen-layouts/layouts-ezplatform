401b6691b649ab72aa008022f77b2d1e49023c43
Edi Modrić <edi.modric@gmail.com>
2018-08-21 11:20:00 +0200

    Revert "Use content extractor in context provider"
    This reverts commit 996a5310b4bfe4c6e36ec53f9daef3ffea271fda
    
    Context provider is executed way to early for content extractor
    to be able to run.

--- lib/Context/ContextProvider.php
+++ lib/Context/ContextProvider.php
@@ -5,27 +5,22 @@ declare(strict_types=1);
 namespace Netgen\BlockManager\Ez\Context;
 
 use eZ\Publish\API\Repository\Values\Content\Location;
+use eZ\Publish\Core\MVC\Symfony\Routing\UrlAliasRouter;
+use eZ\Publish\Core\MVC\Symfony\View\LocationValueView;
 use Netgen\BlockManager\Context\ContextInterface;
 use Netgen\BlockManager\Context\ContextProviderInterface;
-use Netgen\BlockManager\Ez\ContentProvider\ContentExtractorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 final class ContextProvider implements ContextProviderInterface
 {
     /**
-     * @var \Netgen\BlockManager\Ez\ContentProvider\ContentExtractorInterface
-     */
-    private $contentExtractor;
-
-    /**
      * @var \Symfony\Component\HttpFoundation\RequestStack
      */
     private $requestStack;
 
-    public function __construct(ContentExtractorInterface $contentExtractor, RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack)
     {
-        $this->contentExtractor = $contentExtractor;
         $this->requestStack = $requestStack;
     }
 
@@ -36,10 +31,37 @@ final class ContextProvider implements ContextProviderInterface
             return;
         }
 
-        $location = $this->contentExtractor->extractLocation($currentRequest);
+        $locationId = null;
 
-        if ($location instanceof Location) {
-            $context->set('ez_location_id', (int) $location->id);
+        // Normally, here we would use request content extractor to
+        // extract the location from the request, but since currently
+        // it only supports extracting the content from "view" attribute,
+        // it cannot be used here since this is executed way before "view"
+        // attribute is even populated in the request.
+
+        // @todo Consider refactoring the context builder to allow for some
+        // kind of provider fallback.
+
+        if ($currentRequest->attributes->has('view')) {
+            $view = $currentRequest->attributes->get('view');
+            if ($view instanceof LocationValueView) {
+                $locationId = $view->getLocation()->id;
+            }
+        } elseif ($currentRequest->attributes->has('location')) {
+            $location = $currentRequest->attributes->get('location');
+            if ($location instanceof Location) {
+                $locationId = $location->id;
+            }
+        } elseif ($currentRequest->attributes->has('locationId')) {
+            $currentLocationId = $currentRequest->attributes->get('locationId');
+            $currentRoute = $currentRequest->attributes->get('_route');
+            if ($currentLocationId !== null && $currentRoute === UrlAliasRouter::URL_ALIAS_ROUTE_NAME) {
+                $locationId = $currentLocationId;
+            }
+        }
+
+        if ($locationId !== null) {
+            $context->set('ez_location_id', (int) $locationId);
         }
     }
 }
